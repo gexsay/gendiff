78908054269e398936e1e2f723351e1edc00ce4d
Nikita Olegovich <xdoku@yandex.ru>
2023-05-03 20:40:50 +0500

    update cli

--- gendiff/cli.py
+++ gendiff/cli.py
@@ -3,10 +3,9 @@ import argparse
 
 def main():
     parser = argparse.ArgumentParser(
-        usage="gendiff [-h] first_file second_file",
-        description="Compares two configuration files and shows a difference.",
+        description="Compares two configuration files and shows a difference."
     )
     parser.add_argument("first_file")
     parser.add_argument("second_file")
-
-    print(parser.print_help())
+    parser.add_argument("-f", "--format", help="set format of output")
+    args = parser.parse_args()
