f2dbcc713aaf16d19a83eaaa652acf4ba77e664e
Nikita Olegovich <xdoku@yandex.ru>
2023-05-05 15:11:28 +0500

    check tests

--- tests/test_flat_json.py
+++ tests/test_flat_json.py
@@ -1,17 +1,25 @@
+import pytest
+import os
 import json
 from gendiff.cli import gendiff_diff
 
-
-def read(file):
-    with open(file, 'r') as file:
-        return json.load(file)
+func_name_length = len("test_output_first")
+file_name_length = len(__file__)
+file_path = __file__[:file_name_length - func_name_length]
 
 
 def test_output_first():
-    with open("/home/nikita/VScode/gendiff/tests/fixtures/json_files/first.json") as first_json:
-        with open ("/home/nikita/VScode/gendiff/tests/fixtures/json_files/second.json") as second_json:
-            with open ("/home/nikita/VScode/gendiff/tests/fixtures/output_files/first_second.txt") as output:
+    path_first = os.path.abspath(file_path + "fixtures/json_files/first.json")
+    path_second = os.path.abspath(file_path + "fixtures/json_files/second.json")
+    path_output = os.path.abspath(file_path + "fixtures/output_files/first_second.txt")
+    with open(path_first) as first_json:
+        with open(path_second) as second_json:
+            with open(path_output) as output:
                 first = json.load(first_json)
                 second = json.load(second_json)
-                gendiff = gendiff_diff(first, second)
-                assert output == gendiff
+                content = output.read()
+                func_output = (gendiff_diff(first, second))
+                assert content == func_output
+
+
+test_output_first()
